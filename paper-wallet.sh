8a4eeace1cfa182ba49a5cef9f5197e536ed6aec
gehlm <mjgehl@gmail.com>
2013-11-26 10:54:25 -0600

    Dialog messages changed for consistancy.

--- paper-wallet.sh
+++ paper-wallet.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+#
+# Script to generate offline Bitcoin addresses 
+# 
+# Requires libbitcoin and sx
+# https://github.com/spesmilo/libbitcoin/
+# https://github.com/spesmilo/sx
+#
+# Console dialogs:
+# http://invisible-island.net/dialog/
+#
+# Qrcode generation: 
+# http://fukuchi.org/works/qrencode/
+
 
 # Generate a new seed
 new_seed(){
@@ -9,12 +22,12 @@ new_seed(){
 # Convert seed to 12 words
 display_mnemonic(){
 	mnemonic=$(echo $m_seed | sx "mnemonic")
-	dialog --backtitle "Console Paper Wallet: Mnemonic Seed" --title "Electrum Compatible 12 Word mnemonic" --msgbox "\n$mnemonic" 9 50
+	dialog --backtitle "Console Paper Wallet: Mnemonic Seed" --title "Electrum compatible 12-word mnemonic" --msgbox "\n$mnemonic" 9 50
 }
 
 # Convert 12 words to seed
 mnemonic_to_seed(){
-	cmd=(dialog --backtitle "Console Paper Wallet: Mnemonic Seed Entry"  --title "Electrum 12-word mnemonic" --inputbox "Enter 12 word mnemonic" 8 75)
+	cmd=(dialog --backtitle "Console Paper Wallet: Mnemonic Seed Entry"  --title "Electrum compatible 12-word mnemonic" --inputbox "Enter 12 word mnemonic:" 8 75)
 
 	while :
 	do
@@ -26,9 +39,9 @@ mnemonic_to_seed(){
 		fi
 
 		if [ $word_count -eq 12 ]; then
+			# Verify mnemonic
 			tmp_seed=$(echo $words | sx "mnemonic" | tr -d '\n')
-			mnemonic=$(echo $tmp_seed | sx "mnemonic")
-			# Verify mnemonic		
+			mnemonic=$(echo $tmp_seed | sx "mnemonic")	
 			if [ "$words" == "$mnemonic" ]; then
 				m_seed=$tmp_seed
 				break 2
@@ -36,16 +49,16 @@ mnemonic_to_seed(){
 				dialog --backtitle "Console Paper Wallet: Error" --title "mnemonic error" --msgbox "Invalid mnemonic. Try again." 9 50
 			fi	
 		else
-			dialog --backtitle "Console Paper Wallet: Error" --title "mnemonic error" --msgbox "Mnemonic must be 12 words" 9 50
+			dialog --backtitle "Console Paper Wallet: Error" --title "mnemonic error" --msgbox "Mnemonic must be 12 words seperated by spaces." 9 50
 		fi
 		
 	done
 }
 
-# Display Public Keys
-pub_key(){
+# Display Public Address
+pub_addr(){
 	re='^[0-9]+$'
-	cmd=(dialog --backtitle "Console Paper Wallet: Public Key Select" --keep-tite --title "Key Index" --inputbox "Which Public Key Number?" 10 30)
+	cmd=(dialog --backtitle "Console Paper Wallet: Public Address Select" --keep-tite --title "Address Index" --inputbox "Which public address number?" 10 36)
 	while :
 	do
 		index=$("${cmd[@]}" 2>&1 >/dev/tty)	
@@ -53,8 +66,7 @@ pub_key(){
 			pub_key=$(echo $m_seed | sx "genaddr" $index)
 			qr_code=$(qrencode -s 10 -m 1 -t ASCII "$pub_key")		
 			qr_unicode=${qr_code//"#"/$unicode_box_char}
-			#echo $qr_unicode
-			dialog --backtitle "Console Paper Wallet: Public Key" --no-collapse --keep-tite --title "Key[$index]: $pub_key" --msgbox "$qr_unicode" 36 66
+			dialog --backtitle "Console Paper Wallet: Public Address" --no-collapse --keep-tite --title "Key[$index]: $pub_key" --msgbox "$qr_unicode" 36 66
 		  	break;
 		else break;
 		fi
@@ -64,7 +76,7 @@ pub_key(){
 # Display Private Key
 priv_key(){
 	re='^[0-9]+$'
-	cmd=(dialog --backtitle "Console Paper Wallet: Private Key Select" --keep-tite --title "Key Index" --inputbox "Which Private Key Number?" 10 30)
+	cmd=(dialog --backtitle "Console Paper Wallet: Private Key Select" --keep-tite --title "Key Index" --inputbox "Which private key number?" 10 36)
 	while :
 	do
 		index=$("${cmd[@]}" 2>&1 >/dev/tty)	
@@ -84,7 +96,7 @@ main_menu(){
 
 	while :
 	do
-		cmd=(dialog --backtitle "Console Paper Wallet: Main Menu" --keep-tite --no-cancel --menu "Current Seed: $m_seed" 18 70 22)
+		cmd=(dialog --backtitle "Console Paper Wallet: Main Menu" --keep-tite --no-cancel --menu "Current Seed: $m_seed" 14 55 22)
 
 		options=(1 "Show mnemonic"
 				 2 "Show public address"
@@ -103,7 +115,7 @@ main_menu(){
 					break
 				    ;;
 				2)
-					pub_key
+					pub_addr
 					break
 				    ;;
 				3)
